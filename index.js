316f3b98d4aa5bd19aa088b347a07a997b60a2b5
Thomas Jaggi <thomas.jaggi@unic.com>
2017-11-20 16:47:28 +0100

    Replace StandardJS with Airbnb config, lint files, add missing dependencies

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const gulp = require('gulp');
+const prettify = require('gulp-prettify');
+const handlebars = require('gulp-hb');
+const path = require('path');
+const through = require('through2');
+const util = require('gulp-util');
+const importFresh = require('import-fresh');
+const merge = require('lodash.merge');
+
 const defaults = {
   src: [
     './source/*.hbs',
@@ -5,7 +14,7 @@ const defaults = {
     './source/demo/pages/**/*.hbs',
     './source/modules/**/!(_)*.hbs',
     './source/demo/modules/**/!(_)*.hbs',
-    './source/preview/styleguide/*.hbs'
+    './source/preview/styleguide/*.hbs',
   ],
   srcBase: './source',
   plugins: {
@@ -14,109 +23,85 @@ const defaults = {
         './source/layouts/*.hbs',
         './source/modules/**/*.hbs',
         './source/demo/modules/**/*.hbs',
-        './source/preview/**/*.hbs'
+        './source/preview/**/*.hbs',
       ],
       parsePartialName: (options, file) => {
-        const path = require('path')
-
-        let filePath = path.relative('./source', file.path)
+        const filePath = path.relative('./source', file.path)
           // Remove extension
           .replace(path.extname(file.path), '')
           // Use forward slashes on every OS
-          .replace(new RegExp('\\' + path.sep, 'g'), '/')
+          .replace(new RegExp(`\\${path.sep}`, 'g'), '/');
 
-        return filePath
-      }
+        return filePath;
+      },
     },
     data: (file) => {
-      const path = require('path')
-
-      let data = {}
+      let data = {};
 
       // Find .data.js file with same name
       try {
-        data = require(file.path.replace(path.extname(file.path), '.data.js'))
-      } catch (e) {}
+        data = importFresh(file.path.replace(path.extname(file.path), '.data.js'));
+      } catch (e) {} // eslint-disable-line no-empty
 
-      return data
+      return data;
     },
     prettify: {
       indent_with_tabs: false,
-      max_preserve_newlines: 1
-    }
+      max_preserve_newlines: 1,
+    },
   },
   errorHandler: (error) => {
-    const util = require('gulp-util')
-
-    util.log(error.plugin, util.colors.cyan(error.fileName), util.colors.red(error.message))
+    util.log(error.plugin, util.colors.cyan(error.fileName), util.colors.red(error.message));
   },
   dest: './build/',
   watch: [
-    './source/*.(hbs|data\.js|md)',
-    './source/pages/**/*.(hbs|data\.js|md)',
-    './source/demo/pages/**/*.(hbs|data\.js|md)',
-    './source/modules/**/!(_)*.(hbs|data\.js|md)',
-    './source/demo/modules/**/!(_)*.(hbs|data\.js|md)',
-    './source/preview/styleguide/*.(hbs|data\.js|md)'
-  ]
-}
+    './source/*.(hbs|data.js|md)',
+    './source/pages/**/*.(hbs|data.js|md)',
+    './source/demo/pages/**/*.(hbs|data.js|md)',
+    './source/modules/**/!(_)*.(hbs|data.js|md)',
+    './source/demo/modules/**/!(_)*.(hbs|data.js|md)',
+    './source/preview/styleguide/*.(hbs|data.js|md)',
+  ],
+};
 
-const fn = (config, fileEvents, cb) => {
-  const gulp = require('gulp')
-  const prettify = require('gulp-prettify')
-  const handlebars = require('gulp-hb')
-  const path = require('path')
-  const through = require('through2')
+const fn = config => gulp.src(config.src, {
+  base: config.srcBase,
+})
 
-  if (typeof fileEvents === 'function') {
-    cb = fileEvents
-    fileEvents = null
-  }
+  // TODO: Add dependency graph and decide based on fileEvents which files to pass through
+  // .pipe(through.obj((file, enc, done) => {
+  //   done(null, file)
+  // }))
 
-  return gulp.src(config.src, {
-    base: config.srcBase
-  })
+  // Find data and assign it to file object
+  .pipe(through.obj((file, enc, done) => {
+    file.data = config.plugins.data(file); // eslint-disable-line no-param-reassign
 
-    // TODO: Add dependency graph and decide based on fileEvents which files to pass through
-    // .pipe(through.obj((file, enc, done) => {
-    //   done(null, file)
-    // }))
+    done(null, file);
+  }))
 
-    // Find data and assign it to file object
-    .pipe(through.obj((file, enc, done) => {
-      file.data = config.plugins.data(file)
+  // Handlebars
+  .pipe(handlebars(config.plugins.handlebars).on('error', config.errorHandler))
 
-      done(null, file)
-    }))
+  // Formatting
+  .pipe(prettify(config.plugins.prettify))
 
-    // Handlebars
-    .pipe(handlebars(config.plugins.handlebars).on('error', config.errorHandler))
+  // Rename to .html
+  .pipe(through.obj((file, enc, done) => {
+    file.path = file.path.replace(path.extname(file.path), '.html'); // eslint-disable-line no-param-reassign
 
-    // Formatting
-    .pipe(prettify(config.plugins.prettify))
+    done(null, file);
+  }))
 
-    // Rename to .html
-    .pipe(through.obj((file, enc, done) => {
-      file.path = file.path.replace(path.extname(file.path), '.html')
-
-      done(null, file)
-    }))
-
-    // Save
-    .pipe(gulp.dest(config.dest))
-
-    // (Optional) callback
-    .on('finish', cb || (() => {}))
-}
+  // Save
+  .pipe(gulp.dest(config.dest));
 
 module.exports = (options) => {
-  const merge = require('lodash.merge')
-
-  const config = merge({}, defaults, options)
+  const config = merge({}, defaults, options);
 
   return {
     defaults,
     config,
-    fn: fn.bind(null, config)
-  }
-}
+    fn: fn.bind(null, config),
+  };
+};
--- test/index.js
+++ test/index.js
@@ -1,56 +1,53 @@
-const glob = require('glob')
-const path = require('path')
-const fs = require('fs')
-const should = require('should') // eslint-disable-line
-const del = require('del')
+const glob = require('glob');
+const path = require('path');
+const fs = require('fs');
+const should = require('should'); // eslint-disable-line no-unused-vars
+const del = require('del');
 
 module.exports = {
-  before: function (done) {
+  before(done) {
     const config = {
       src: './test/fixtures/!(_)*.hbs',
       srcBase: './test/fixtures/',
       dest: './test/results/',
       plugins: {
-        data: (file) => {
+        data: file =>
           // Use JSON file with same name as html file
-          return require(file.path.replace(path.extname(file.path), '.json'))
-        },
+          require(file.path.replace(path.extname(file.path), '.json')), // eslint-disable-line global-require, import/no-dynamic-require
         handlebars: {
           partials: [
-            './test/fixtures/_*.hbs'
+            './test/fixtures/_*.hbs',
           ],
-          parsePartialName: (options, file) => {
-            return path.relative('./test/fixtures', file.path)
-              .replace(path.extname(file.path), '')
-              .replace(new RegExp('\\' + path.sep, 'g'), '/')
-          }
+          parsePartialName: (options, file) => path.relative('./test/fixtures', file.path)
+            .replace(path.extname(file.path), '')
+            .replace(new RegExp(`\\${path.sep}`, 'g'), '/'),
         },
         prettify: {
           // Use tabs over spaces
-          indent_with_tabs: true
-        }
-      }
-    }
+          indent_with_tabs: true,
+        },
+      },
+    };
 
-    const task = require('../index.js')(config)
+    const task = require('../index.js')(config); // eslint-disable-line global-require
 
-    task.fn().on('end', done)
+    task.fn().on('end', done);
   },
 
-  default: function () {
+  default() {
     const expected = glob.sync(path.join(__dirname, '/expected/**/*'), {
-      nodir: true
-    })
+      nodir: true,
+    });
 
     expected.forEach((filePath) => {
-      const expectedFile = fs.readFileSync(filePath).toString()
-      const resultedFile = fs.readFileSync(filePath.replace('expected', 'results')).toString()
+      const expectedFile = fs.readFileSync(filePath).toString();
+      const resultedFile = fs.readFileSync(filePath.replace('expected', 'results')).toString();
 
-      expectedFile.should.be.eql(resultedFile)
-    })
+      expectedFile.should.be.eql(resultedFile);
+    });
   },
 
-  after: function (done) {
-    del(path.join(__dirname, '/results')).then(() => done())
-  }
-}
+  after(done) {
+    del(path.join(__dirname, '/results')).then(() => done());
+  },
+};
