d3e9dbbe1354d997935635036468c7dd98cc81f1
Thomas Jaggi <thomas.jaggi@unic.com>
2017-11-20 19:51:39 +0100

    Replace Mocha with AVA

--- test/index.js
+++ test/index.js
@@ -1,40 +1,36 @@
+const test = require('ava');
 const glob = require('glob');
 const path = require('path');
 const fs = require('fs');
-const should = require('should'); // eslint-disable-line no-unused-vars
 const del = require('del');
 
-module.exports = {
-  before(done) {
-    const config = {
-      src: './test/fixtures/!(_)*.hbs',
-      srcBase: './test/fixtures/',
-      dest: './test/results/',
-      plugins: {
-        data: file =>
-          // Use JSON file with same name as html file
-          require(file.path.replace(path.extname(file.path), '.json')), // eslint-disable-line global-require, import/no-dynamic-require
-        handlebars: {
-          partials: [
-            './test/fixtures/_*.hbs',
-          ],
-          parsePartialName: (options, file) => path.relative('./test/fixtures', file.path)
-            .replace(path.extname(file.path), '')
-            .replace(new RegExp(`\\${path.sep}`, 'g'), '/'),
-        },
-        prettify: {
-          // Use tabs over spaces
-          indent_with_tabs: true,
-        },
+test.cb('default', (t) => {
+  const config = {
+    src: './test/fixtures/!(_)*.hbs',
+    srcBase: './test/fixtures/',
+    dest: './test/results/',
+    plugins: {
+      data: file =>
+        // Use JSON file with same name as html file
+        require(file.path.replace(path.extname(file.path), '.json')), // eslint-disable-line global-require, import/no-dynamic-require
+      handlebars: {
+        partials: [
+          './test/fixtures/_*.hbs',
+        ],
+        parsePartialName: (options, file) => path.relative('./test/fixtures', file.path)
+          .replace(path.extname(file.path), '')
+          .replace(new RegExp(`\\${path.sep}`, 'g'), '/'),
       },
-    };
+      prettify: {
+        // Use tabs over spaces
+        indent_with_tabs: true,
+      },
+    },
+  };
 
-    const task = require('../index.js')(config); // eslint-disable-line global-require
+  const task = require('../index.js')(config); // eslint-disable-line global-require
 
-    task.fn().on('end', done);
-  },
-
-  default() {
+  task.fn().on('end', () => {
     const expected = glob.sync(path.join(__dirname, '/expected/**/*'), {
       nodir: true,
     });
@@ -43,11 +39,14 @@ module.exports = {
       const expectedFile = fs.readFileSync(filePath).toString();
       const resultedFile = fs.readFileSync(filePath.replace('expected', 'results')).toString();
 
-      expectedFile.should.be.eql(resultedFile);
+      t.is(expectedFile, resultedFile);
     });
-  },
 
-  after(done) {
-    del(path.join(__dirname, '/results')).then(() => done());
-  },
-};
+    t.end();
+  });
+});
+
+// Clean up
+test.after(() => {
+  return del(path.join(__dirname, '/results'));
+})
